07b5c3d2e099d2483d6bc21eafc3f3c8308edb09
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-16 15:10:43 +0300

    Update models.py

--- places/models.py
+++ places/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-print('hello')
+class Place(models.Model):
+    title = models.CharField("Название", max_length=200)
+    description_short = models.CharField("Краткое описание", max_length=250)
+    description_long = models.TextField("Большое описание")
+    long = models.DecimalField("Долгота", max_digits=17, decimal_places=14)
+    lat = models.DecimalField("Широта", max_digits=17, decimal_places=14)
+
+    def __str__(self):
+        return self.title
