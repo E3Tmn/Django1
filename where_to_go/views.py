d86404c8b36af9d69e06886b4add38752914ed87
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-17 19:53:28 +0300

    take data from db

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,36 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from places.models import Place, Image
 
 
 def index_page(request):
+    db_places = Place.objects.all()
     places = {
       "type": "FeatureCollection",
-      "features": [
-        {
+      "features": []
+    }
+    for place in db_places:
+      places['features'].append({
           "type": "Feature",
           "geometry": {
             "type": "Point",
-            "coordinates": [37.62, 55.793676]
+            "coordinates": [place.long, place.lat]
           },
           "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
+            "title": place.title,
+            "placeId": place.id,
             "detailsUrl": "../static/places/moscow_legends.json"
           }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "../static/places/roofs24.json"
-          }
-        }
-      ]
-    }
+        })
+
     data = {'places': places}
     return render(request, 'index.html', context=data)
