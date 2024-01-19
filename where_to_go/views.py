dd04a9b11f3e0aeca1f3032f4c4e4eeb1ace0776
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-19 17:42:06 +0300

    add page about place

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from places.models import Place, Image
+
+
+def place_page(request, place_id):
+  place = get_object_or_404(Place, id=place_id)
+  images = Image.objects.filter(place=place)
+  json_response = {
+    "title": place.title,
+    "imgs": [],
+    "description_short": place.description_short,
+    "description_long": place.description_long,
+    "coordinates": {
+        "lng": place.long,
+        "lat": place.lat
+    }
+  }
+  for image in images:
+    json_response['imgs'].append(image.picture.url)
+
+  return JsonResponse(json_response, json_dumps_params={'ensure_ascii': False, 'indent': 2})
 
 
 def index_page(request):
