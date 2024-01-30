1010e8a7dfa500b4d00acca353496929655a3a14
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-30 15:12:25 +0300

    add drag and drop for photo

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Place, Image
 from django.utils.html import format_html
+from adminsortable2.admin import SortableAdminBase, SortableStackedInline
 
 # Register your models here.
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableStackedInline):
     model = Image
     readonly_fields = ['preview_image']
     ordering = ("serial_number",)
@@ -14,7 +15,7 @@ class ImageInline(admin.TabularInline):
         return format_html(f'<img src="{url}" height="200px" />')
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [ImageInline]
    
 
