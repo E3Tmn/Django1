bd0134b7d0d0570447150e3e54aff7b8bb4bb6af
E3Tmn <nick1tapinyagin@yandex.ru>
2024-02-08 10:10:29 +0300

    add default value

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG_MODE')
+DEBUG = env.bool('DEBUG_MODE', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='*')
 
 
 # Application definition
